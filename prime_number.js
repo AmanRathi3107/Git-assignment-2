d633a879b17f2f2699bbce066e990c6aaf2bf649
jitendra-kumar <jitendra0123@gmail.com>
2022-09-13 13:23:22 +0530

    codefir for prime number

--- prime_number.js
+++ prime_number.js
@@ -1,5 +1,8 @@
 let num=5;
-if(num%2==0 && num%3==0){
+if(num==1){
+    console.log("1 is not a prime not a composite number");
+}
+else if(num%2==0 || num%3==0){
     console.log("Not a Prime Number")
 }
 else{
